288bed49252d27476257581b36c52cb14154f3fe
Oleksandr <sashaka2013@gmail.com>
2020-01-30 00:08:11 +0200

    Updates

---
+++ precache-manifest.9fb73b04d37ac939103bdcac74eb0b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9daab1455395ff0252a9911a8ab309fb",
+    "url": "/game-in-dots/index.html"
+  },
+  {
+    "revision": "3acdd2b44962552cbf64",
+    "url": "/game-in-dots/static/css/main.1e4d29b3.chunk.css"
+  },
+  {
+    "revision": "cca0c5312bdff41a045a",
+    "url": "/game-in-dots/static/js/2.21a6e458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-in-dots/static/js/2.21a6e458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3acdd2b44962552cbf64",
+    "url": "/game-in-dots/static/js/main.669001ae.chunk.js"
+  },
+  {
+    "revision": "b4db3f4fcdecb4a4ea8c",
+    "url": "/game-in-dots/static/js/runtime-main.63a37834.js"
+  }
+]);
