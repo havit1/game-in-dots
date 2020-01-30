de40090f167c528171653245180e4c513329c08f
Oleksandr <sashaka2013@gmail.com>
2020-01-30 17:12:09 +0200

    Updates

---
+++ precache-manifest.b29c61b48f494bbc5b95ce87d8466978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043cd9010a0df5b52864aa4dabd5c407",
+    "url": "/game-in-dots/index.html"
+  },
+  {
+    "revision": "9990ab35752cec53a4ec",
+    "url": "/game-in-dots/static/css/main.2e4342dd.chunk.css"
+  },
+  {
+    "revision": "258b063f94e921eb5d0d",
+    "url": "/game-in-dots/static/js/2.fc1ff09d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-in-dots/static/js/2.fc1ff09d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9990ab35752cec53a4ec",
+    "url": "/game-in-dots/static/js/main.7a11f150.chunk.js"
+  },
+  {
+    "revision": "b4db3f4fcdecb4a4ea8c",
+    "url": "/game-in-dots/static/js/runtime-main.63a37834.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-in-dots/precache-manifest.74d029153d26a2511e7441a1e37fface.js"
+  "/game-in-dots/precache-manifest.b29c61b48f494bbc5b95ce87d8466978.js"
 );
 
 self.addEventListener('message', (event) => {
